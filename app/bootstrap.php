0db7b3c3ed6be4cacb65fb78efe1af85a201b40c
Rossana Liendo <rossana.liendo@gmail.com>
2024-05-28 12:41:32 +0200

    cretaing the routes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,9 @@
-//xd
+<?php
+
+//For autoloading classes
+spl_autoload_register(function ($className) {
+    $path = "core/$className.php";
+    if (file_exists($path)) {
+        require_once $path;
+    }
+});
---
+++ app/core/App.php
@@ -0,0 +1,7 @@
+<?php
+
+class App {
+    private $controller = 'HomeController';
+    private $method = 'index';
+    private $params = [];
+}
