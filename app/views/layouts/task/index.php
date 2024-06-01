36ef752c05f5f378486765878771c60b054a59ce
Rossana Liendo <rossana.liendo@gmail.com>
2024-06-01 23:00:11 +0200

    Creating the index for tasks

---
+++ app/views/layouts/task/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Lista de Tareas</title>
+        <link href="/web/styles.css" rel="stylesheet">    
+    </head>
+    <body class="bg-gray-100">
+        <div class="container mx-auto p-4">
+            <h1 class="text-2xl font-bold mb-4">Lista de tareas</h1>
+        </div>
+
+    </body>
+
+</html>
