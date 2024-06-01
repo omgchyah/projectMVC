3e008dffbdb1fd8e4c395069ec57e8a0d6bc7d1d
Rossana Liendo <rossana.liendo@gmail.com>
2024-06-01 23:12:50 +0200

    created my tailwind.css

--- app/views/layouts/task/index.php
+++ app/views/layouts/task/index.php
@@ -8,6 +8,9 @@
     <body class="bg-gray-100">
         <div class="container mx-auto p-4">
             <h1 class="text-2xl font-bold mb-4">Lista de tareas</h1>
+            <?php
+            echo '<h1 class="bg-blue-300">Hello, PHP</h1>';
+            ?>
         </div>
 
     </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/views/**/*.phtml',
+    './app/views/**/*.{html,js,php}',
     './web/**/*.html',
     './web/**/*.php',
   ],
