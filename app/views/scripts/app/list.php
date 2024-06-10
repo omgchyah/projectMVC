de119ed9b349638ffd8a15affc9b1013fa7bf147
Rossana Liendo <rossana.liendo@gmail.com>
2024-06-10 22:57:53 +0200

    Commenting the debugging echoes

--- app/views/scripts/app/list.php
+++ app/views/scripts/app/list.php
@@ -3,18 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Task List</title>
-    <link href="/PHP/ProjectMVC/web/stylesheets/styles.css" rel="stylesheet">
-    <!-- Include jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <!-- Include tablesorter plugin -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js"></script>
-    <!-- Include tablesorter CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/css/theme.default.min.css">
-    <script>
-        $(document).ready(function() {
-            $("#sortable-table").tablesorter();
-        });
-    </script>
 </head>
 <body>
 
